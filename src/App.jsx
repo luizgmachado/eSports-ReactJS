1098b64d9d7156e4bd1c415131fb872747f39167
luizgmachado <luiz.gustavo.machado@hotmail.com>
2023-04-17 20:51:52 -0300

    correction channel

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ const channelListData = [
 	{
 		url: "https://www.twitch.tv/birobirobiro",
 		imageUrl:
-			"https://static-cdn.jtvnw.net/jtv_user_pictures/7b74d2c9-e31d-4237-b41e-8426892415e5-profile_image-150x150.jpeg",
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/46e73d88-ed0a-4f47-ab56-e14c61dff6d8-profile_image-150x150.png",
 		alt: "Foto de Perfil BiroBiroBiro",
 	},
 
@@ -89,7 +89,7 @@ const channelListData = [
 	{
 		url: "https://www.twitch.tv/dabicarp",
 		imageUrl:
-			"https://static-cdn.jtvnw.net/jtv_user_pictures/70b5c9b7-49df-42bf-af63-55d9295c4067-profile_image-150x150.jpeg",
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/7b74d2c9-e31d-4237-b41e-8426892415e5-profile_image-150x150.jpeg",
 		alt: "imagem dabicarp",
 	},
 
